99e383b40240b5489bb80ff40c95e3e043682802
Ofir Dagan <ofirdagan@gmail.com>
2016-02-24 23:06:53 +0200

    step1: android only

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,33 +12,11 @@ var app = require("application");
 export class AppComponent {
   public label = 'Hello Bash';
 
-  constructor(private ngZone: NgZone) {
-
+  constructor() {
   }
 
   showConfirmDialog() {
-    if (app.android) {
-      this.confirmAndroid();
-    } else if (app.ios) {
-      this.confirmIphone();
-    }
-    // Or, just use nartivescript dialogs module:
-    //this.confirm();
-  }
-
-  confirm() {
-    var options = {
-      title: 'Survey',
-      message: 'Am I boring you?',
-      okButtonText: "Yes",
-      cancelButtonText: "Hell Yes"
-    };
-    dialogs.confirm(options).then(result => {
-      this.label = result ? 'Yes Clicked!' : 'Hell Yes Clicked!';
-    }).catch(e => {
-      console.log(e);
-    });
-
+    this.confirmAndroid();
   }
 
   confirmAndroid() {
@@ -49,39 +27,18 @@ export class AppComponent {
       .setMessage('Am I boring you?')
       .setPositiveButton('Yes', new android.content.DialogInterface.OnClickListener({
         onClick: (dialog, id: number) => {
-          this.ngZone.run(() => {
-            dialog.cancel();
-            this.label = 'Yes Clicked!';
-          });
+          dialog.cancel();
+          this.label = 'Yes Clicked!';
         }
       }))
       .setNegativeButton('Hell Yes', new android.content.DialogInterface.OnClickListener({
         onClick: (dialog, id: number) => {
-          this.ngZone.run(() => {
-            dialog.cancel();
-            this.label = 'Hell Yes Clicked';
-          });
+          dialog.cancel();
+          this.label = 'Hell Yes Clicked';
         }
       }));
 
     builder.create();
     builder.show();
-  }
-
-
-  confirmIphone() {
-    //API GUIDE: https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIAlertController_class/
-
-    let alertController = UIAlertController
-      .alertControllerWithTitleMessagePreferredStyle('Survey', 'Am I boring you?', UIAlertControllerStyle.UIAlertControllerStyleAlert);
-
-    alertController.addAction(UIAlertAction.actionWithTitleStyleHandler('Yes', UIAlertActionStyle.UIAlertActionStyleDefault, (arg) => {
-      this.ngZone.run(() => this.label = 'Yes Clicked!');
-    }));
-    alertController.addAction(UIAlertAction.actionWithTitleStyleHandler('Hell Yes', UIAlertActionStyle.UIAlertActionStyleDefault, (arg) => {
-      this.ngZone.run(() => this.label = 'Hell Yes Clicked!');
-    }));
-    var viewController = app.ios.rootController;
-    viewController.presentModalViewControllerAnimated(alertController, true);
   }
 }
